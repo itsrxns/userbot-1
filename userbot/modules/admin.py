73dfd9c1d816eb10ea1a6bbc01cafb06dc787044
Shanu Flash #noυ <shanuflash@gmail.com>
2019-02-20 12:35:53 +0530

    Ooops

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import sqlite3
-import time
-
-from telethon import TelegramClient, events
-from telethon.errors import (ChannelInvalidError, ChatAdminRequiredError,
-                             UserAdminInvalidError)
-from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest
-from telethon.tl.types import ChatAdminRights, ChatBannedRights
-=======
 from time import sleep
 
 from telethon import events
@@ -18,23 +8,12 @@ from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest
 from telethon.tl.types import ChatAdminRights, ChatBannedRights
 
 from userbot import (BRAIN_CHECKER, LOGGER, LOGGER_GROUP, bot)
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
 
 from userbot import (BRAIN_CHECKER, LOGGER, LOGGER_GROUP, SPAM, SPAM_ALLOWANCE,
                      bot)
 
-<<<<<<< HEAD
 @bot.on(events.NewMessage(outgoing=True, pattern="^.upromote$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.upromote$"))
-async def wizzard(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        chats=await e.get_chat()
-        rights = chats.admin_rights
-        rights3 = chats.creator
-        rights2 = ChatAdminRights(
-=======
-@bot.on(events.NewMessage(outgoing=True, pattern="^.promote$"))
-@bot.on(events.MessageEdited(outgoing=True, pattern="^.promote$"))
 async def promote(promt):
     """ For .promote command, do promote targeted person """
     if not promt.text[0].isalpha() \
@@ -43,7 +22,6 @@ async def promote(promt):
         admin = chats.admin_rights
         creator = chats.creator
         new_rights = ChatAdminRights(
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             add_admins=True,
             invite_users=True,
             change_info=True,
@@ -68,42 +46,18 @@ async def promote(promt):
                                  (await promt.get_reply_message()).sender_id,
                                  rights)
             )
-<<<<<<< HEAD
-        except Exception:
-            await e.edit("`You Don't have sufficient permissions to paramod`")
-=======
-
         # If Telethon spit BadRequestError, assume
         # we don't have Promote permission
         except BadRequestError:
             await promt.edit(
                 "`You Don't have sufficient permissions to parmod`"
                 )
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             return
         await promt.edit("`Promoted Successfully!`")
 
 
 @bot.on(events.NewMessage(outgoing=True, pattern="^.demote$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.demote$"))
-<<<<<<< HEAD
-async def demote(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        rights = ChatAdminRights(
-            add_admins=False,
-            invite_users=False,
-            change_info=False,
-            ban_users=False,
-            delete_messages=False,
-            pin_messages=False,
-            invite_link=False,
-        )
-        chat=await e.get_chat()
-        rights = chat.admin_rights
-        rights2 = chat.creator
-        if not (await e.get_reply_message()):
-            await e.edit("`Give a reply message`")
-=======
 async def demote(dmod):
     """ For .demote command, do demote targeted person """
     if not dmod.text[0].isalpha() and dmod.text[0] not in ("/", "#", "@", "!"):
@@ -116,7 +70,6 @@ async def demote(dmod):
         # If there's no reply, return
         if not await dmod.get_reply_message():
             await dmod.edit("`Give a reply message`")
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             return
         # If not admin and not creator, also return
         if not admin and not creator:
@@ -142,34 +95,21 @@ async def demote(dmod):
                                  (await dmod.get_reply_message()).sender_id,
                                  newrights)
             )
-<<<<<<< HEAD
-        except Exception:
-            await e.edit("`You Don't have sufficient permissions to demhott`")
-=======
 
         # If we catch BadRequestError from Telethon
         # Assume we don't have permission to demote
         except BadRequestError:
             await dmod.edit("`You Don't have sufficient permissions to demhott`")
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             return
         await dmod.edit("`Demoted Successfully!`")
 
 
-<<<<<<< HEAD
 @bot.on(events.NewMessage(outgoing=True, pattern="^.uban$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.uban$"))
-async def thanos(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        rights = ChatBannedRights(
-=======
-@bot.on(events.NewMessage(outgoing=True, pattern="^.ban$"))
-@bot.on(events.MessageEdited(outgoing=True, pattern="^.ban$"))
 async def thanos(bon):
     """ For .ban command, do "thanos" at targeted person """
     if not bon.text[0].isalpha() and bon.text[0] not in ("/", "#", "@", "!"):
         banned_rights = ChatBannedRights(
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             until_date=None,
             view_messages=True,
             send_messages=True,
@@ -259,16 +199,8 @@ async def nothanos(unbon):
             await unbon.edit("`Uh oh my unban logic broke!`")
 
 
-<<<<<<< HEAD
 @bot.on(events.NewMessage(outgoing=True, pattern="^.umute$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.umute$"))
-async def spider(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        if (await e.get_reply_message()).sender_id in BRAIN_CHECKER:
-            await e.edit("`Mute Error! I am not supposed to mute this user`")
-=======
-@bot.on(events.NewMessage(outgoing=True, pattern="^.mute$"))
-@bot.on(events.MessageEdited(outgoing=True, pattern="^.mute$"))
 async def spider(spdr):
     """
     This function basically muting peeps
@@ -278,18 +210,13 @@ async def spider(spdr):
         # If the targeted user is a Sudo
         if (await spdr.get_reply_message()).sender_id in BRAIN_CHECKER:
             await spdr.edit("`Mute Error! I am not supposed to mute this user`")
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             return
 
         # Check if the function running under SQL mode
         try:
             from userbot.modules.sql_helper.spam_mute_sql import mute
         except Exception:
-<<<<<<< HEAD
-            await e.edit("`Running on Non-SQL mode!`")
-=======
             await spdr.edit("`Running on Non-SQL mode!`")
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             return
 
         # Get the targeted chat
@@ -320,35 +247,7 @@ async def spider(spdr):
                 + " was muted.",
             )
 
-<<<<<<< HEAD
-
-@bot.on(events.NewMessage(incoming=True, pattern="<triggerban>"))
-async def triggered_ban(e):
-    ban_id = int(e.text[13:])
-    if e.sender_id in BRAIN_CHECKER:  # non-working module#
-        rights = ChatBannedRights(
-            until_date=None,
-            view_messages=True,
-            send_messages=True,
-            send_media=True,
-            send_stickers=True,
-            send_gifs=True,
-            send_games=True,
-            send_inline=True,
-            embed_links=True,
-        )
-    if ban_id in BRAIN_CHECKER:
-        await e.edit("`Sorry Master!`")
-        return
-    await e.edit("`Command from my Master!`")
-    time.sleep(5)
-    await bot(EditBannedRequest(e.chat_id, ban_id, rights))
-    await e.delete()
-    await bot.send_message(e.chat_id, "Job was done, Master! Gimme Cookies!")
-
-
-=======
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
+            
 @bot.on(events.NewMessage(outgoing=True, pattern="^.unmute$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.unmute$"))
 async def unmoot(unmot):
@@ -413,14 +312,9 @@ async def muter(moot):
 
 @bot.on(events.NewMessage(outgoing=True, pattern="^.ungmute$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.ungmute$"))
-<<<<<<< HEAD
-async def ungmute(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-=======
 async def ungmoot(ungmoot):
     if not ungmoot.text[0].isalpha() and ungmoot.text[0] \
             not in ("/", "#", "@", "!"):
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
         try:
             from userbot.modules.sql_helper.gmute_sql import ungmute
         except:
@@ -431,17 +325,10 @@ async def ungmoot(ungmoot):
 
 @bot.on(events.NewMessage(outgoing=True, pattern="^.gmute$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.gmute$"))
-<<<<<<< HEAD
-async def gmute(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        if (await e.get_reply_message()).sender_id in BRAIN_CHECKER:
-            await e.edit("`Mute Error! Couldn't mute this user`")
-=======
 async def gspider(gspdr):
     if not gspdr.text[0].isalpha() and gspdr.text[0] not in ("/", "#", "@", "!"):
         if (await gspdr.get_reply_message()).sender_id in BRAIN_CHECKER:
             await gspdr.edit("`Mute Error! Couldn't mute this user`")
->>>>>>> bb043a4e9d013d23ca853b453a9602df1b128f61
             return
         try:
             from userbot.modules.sql_helper.gmute_sql import gmute
