dcf26cc3e824fb73902121edf57341edce65c399
Shrimadhav U K <ovhvps@shrimadhavuk.me>
2019-03-10 08:37:54 +0000

    Extras: Added .moon animation XD
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>

---
+++ userbot/modules/extras.py
@@ -0,0 +1,71 @@
+import asyncio, subprocess
+import time
+from userbot import bot, LOGGER, LOGGER_GROUP
+from telethon import events
+from telethon.events import StopPropagation
+from telethon.tl.functions.messages import ExportChatInviteRequest
+from telethon.tl.functions.contacts import BlockRequest
+from telethon.tl.functions.channels import LeaveChannelRequest, CreateChannelRequest, DeleteMessagesRequest
+from lmgtfy import lmgtfy
+from collections import deque
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^.leave$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^.leave$"))
+async def leave(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("`I iz Leaving dis Group kek!`")
+        time.sleep(3)
+        if '-' in str(e.chat_id):
+            await bot(LeaveChannelRequest(e.chat_id))
+        else:
+            await e.edit('`Sar This is Not A Chat`')
+
+@bot.on(events.NewMessage(pattern="^.lmg", outgoing=True))
+@bot.on(events.MessageEdited(pattern="^.lmg", outgoing=True))
+async def let_me_google_that_for_you(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        textx = await e.get_reply_message()
+        message = e.text
+        if message[8:]:
+            message = str(message[8:])
+        elif textx:
+            message = str(textx.message)
+        reply_text = 'http://lmgtfy.com/?s=g&iie=1&q=' + message.replace(" ", "+")
+        await e.edit(reply_text)
+        if LOGGER:
+            await bot.send_message(
+                LOGGER_GROUP,
+                "LMGTFY query " + message + " was executed successfully",
+            )
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^;__;$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^;__;$"))
+async def fun(e):
+    t = ";__;"
+    for j in range(10):
+        t = t[:-1] + "_;"
+        await e.edit(t)
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^.cry$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^.cry$"))
+async def cry(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("(;´༎ຶД༎ຶ)")
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^.fp$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^.fp$"))
+async def facepalm(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("🤦‍♂")
+
+@bot.on(events.NewMessage(pattern=r"\.moon animation", outgoing=True))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+	for _ in range(32):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+
+
